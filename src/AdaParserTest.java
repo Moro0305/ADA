a59d1ddbfd01554c623e77eb1930ec1e39e411a5
Ricardo <rmoroyoqui@cetys.edu.mx>
2025-09-18 22:30:10 -0700

    Final changes 2

--- src/AdaParserTest.java
+++ src/AdaParserTest.java
@@ -53,24 +53,50 @@ public class AdaParserTest {
 
     private static String getHardcodedAdaCode() {
         return
-                "with Ada.Text_IO;\n" +
-                        "with Ada.Integer_Text_IO;\n" +
-                        "\n" +
-                        "procedure Ejemplo_Operacion is\n" +
+                "with Ada.Text_IO;         use Ada.Text_IO;\n" +
+                        "with Ada.Integer_Text_IO; use Ada.Integer_Text_IO;\n" +
+                        "procedure Ejemplo_Bucles is\n" +
                         "   -- Declaración de variables\n" +
-                        "   Numero_Uno   : Integer := 10;\n" +
-                        "   Numero_Dos   : Integer := 5;\n" +
-                        "   Suma_Total   : Integer;\n" +
-                        "\n" +
+                        "   Contador     : Integer := 10;\n" +
+                        "   Encontrado   : Boolean := False;\n" +
+                        "   Numero_Buscado: Integer := 3;\n" +
                         "begin\n" +
-                        "   -- Operación de suma\n" +
-                        "   Suma_Total := Numero_Uno + Numero_Dos;\n" +
-                        "\n" +
-                        "   -- Impresión del resultado\n" +
-                        "   Ada.Text_IO.Put(\"La suma es: \");\n" +
-                        "   Ada.Integer_Text_IO.Put(Suma_Total);\n" +
-                        "   Ada.Text_IO.New_Line;\n" +
-                        "\n" +
-                        "end Ejemplo_Operacion;";
+                        "   -- Bucle FOR: Cuenta regresiva de 10 a 1\n" +
+                        "   Put_Line(\"--- Bucle FOR: Cuenta regresiva ---\");\n" +
+                        "   for I in reverse 1..10 loop\n" +
+                        "      Put(I);\n" +
+                        "      Put(\" \");\n" +
+                        "   end loop;\n" +
+                        "   New_Line;\n" +
+                        "   ---\n" +
+                        "   -- Bucle WHILE: Busca un número\n" +
+                        "   Put_Line(\"--- Bucle WHILE: Buscando el numero 3 ---\");\n" +
+                        "   while (Contador >= 1) and (not Encontrado) loop\n" +
+                        "      Put(\"Buscando en: \");\n" +
+                        "      Put(Contador);\n" +
+                        "      New_Line;\n" +
+                        "      if Contador = Numero_Buscado then\n" +
+                        "         Encontrado := True;\n" +
+                        "      end if;\n" +
+                        "      Contador := Contador - 1;\n" +
+                        "   end loop;\n" +
+                        "   if Encontrado then\n" +
+                        "      Put_Line(\"¡Número encontrado!\");\n" +
+                        "   else\n" +
+                        "      Put_Line(\"Número no encontrado.\");\n" +
+                        "   end if;\n" +
+                        "   ---\n" +
+                        "   -- Bucle LOOP: Bucle simple que se ejecuta hasta que la condición se cumple\n" +
+                        "   Put_Line(\"--- Bucle LOOP: Tarea simple ---\");\n" +
+                        "   Contador := 1;\n" +
+                        "   loop\n" +
+                        "      Put(\"Iteracion: \");\n" +
+                        "      Put(Contador);\n" +
+                        "      New_Line;\n" +
+                        "      exit when (Contador = 5) or (Encontrado = True);\n" +
+                        "      Contador := Contador + 1;\n" +
+                        "   end loop;\n" +
+                        "   Put_Line(\"Fin del programa.\");\n" +
+                        "end Ejemplo_Bucles;";
     }
 }
